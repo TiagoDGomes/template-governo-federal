37accc8804e544ea82f5c01789c572564bc544cd
Tiago Gomes <tiago.gomes@ifsp.edu.br>
2018-05-14 11:57:44 -0300

    Acessibilidade

--- resources/js/acessibilidade.js
+++ resources/js/acessibilidade.js
@@ -1,40 +1,44 @@
-window.acessibilidade = {}
+window.acessibilidade = function(){};
 
-window.acessibilidade.ativo = null;
+window.acessibilidade.altoContraste = function(){};
 
-window.acessibilidade.alternar = function () {
-    var className = 'contraste';    
-    var ativar;
-    var cookie_atual = getCookie('layout_classes');
-    if (window.acessibilidade.ativo === null){
-        ativar = cookie_atual.indexOf('contraste') >= 0;
-        
-    } else {  
-        ativar = !window.acessibilidade.ativo;   
-        if (ativar){             
-            if (cookie_atual.indexOf('contraste') < 0){
-                setCookie('layout_classes', cookie_atual + ' contraste' );
-            }
-        } else {
-            setCookie('layout_classes', cookie_atual.replace('contraste',''));
-        }      
-                    
-    }
-    
-    console.log(ativar);
-    if (document.body.classList) {
-        if (ativar) {
-            document.body.classList.add(className);
-        } else {
-            document.body.classList.remove(className);
-        }
-    } else {
-        if (ativar) {
-            document.body.className += ' ' + className;
-        } else {
-            document.body.className = document.body.className.replace(className, '');
-        }
-    }
-    window.acessibilidade.ativo = ativar;
+window.acessibilidade.altoContraste.ativo = null;
+
+window.acessibilidade.altoContraste.classNameValue = 'contraste';
+
+window.acessibilidade.altoContraste.getSetting = function(){
+    var cookieValue = getCookie('layout_classes');
+    return cookieValue.indexOf('contraste') >= 0;
+}
+window.acessibilidade.altoContraste.setSetting = function(value){
+    var className = window.acessibilidade.altoContraste.classNameValue;
+    var cookieValue = getCookie('layout_classes');
+    cookieValue = cookieValue.replace(className, '');
+    setCookie('layout_classes', cookieValue);
+    if (value){
+        setCookie('layout_classes', cookieValue + ' ' + className);
+    } 
+    cookieValue = getCookie('layout_classes');
+       
 }
 
+window.acessibilidade.altoContraste.isActive = function () {
+    return document.body.classList.contains(window.acessibilidade.altoContraste.classNameValue);
+}
+
+window.acessibilidade.altoContraste.setActive = function (value) {
+    var className = window.acessibilidade.altoContraste.classNameValue;
+    if (value) {
+        document.body.classList.add(className);
+    } else {
+        document.body.classList.remove(className);        
+    }
+    console.log("altoContraste:", value);
+    window.acessibilidade.altoContraste.setSetting(value);
+}
+
+window.acessibilidade.altoContraste.setActive(window.acessibilidade.altoContraste.getSetting());
+
+window.acessibilidade.altoContraste.alternar = function(){
+    window.acessibilidade.altoContraste.setActive(!window.acessibilidade.altoContraste.isActive())
+}
