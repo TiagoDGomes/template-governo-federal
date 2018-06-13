42fad849f1327abeda5f39342cb190e8f2840cf5
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-06-13 18:49:18 -0300

    Menu principal em versão mobile

--- resources/js/geral.js
+++ resources/js/geral.js
@@ -1,3 +1,30 @@
 function onLoad() {
     
 }
+function fadeIn(el) {
+    el.style.opacity = 0;  
+    el.style.display = 'block';
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+      last = +new Date();  
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      } 
+    };  
+    tick();
+}
+
+var menuRetratilPosicaoAberta = false;
+function alternarMenuRetratil(){
+    var menuLateral = document.getElementById('menu-lateral');
+    var tituloMenuRetratilA = document.getElementById('titulo-menu-retratil').children[0];
+    if (menuRetratilPosicaoAberta){
+        menuLateral.style.display = 'none';
+        tituloMenuRetratilA.style.backgroundPositionY = 'initial';
+    } else {
+        fadeIn(menuLateral);
+        tituloMenuRetratilA.style.backgroundPositionY = '-73px';
+    }    
+    menuRetratilPosicaoAberta = !menuRetratilPosicaoAberta;
+}
