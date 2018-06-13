bc92343e4c4b82122070b119365eca44d4b808f0
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2018-06-13 19:33:54 -0300

    Usando class CSS para exibição de menu em mobile

--- resources/js/geral.js
+++ resources/js/geral.js
@@ -1,30 +1,13 @@
 function onLoad() {
     
 }
-function fadeIn(el) {
-    el.style.opacity = 0;  
-    el.style.display = 'block';
-    var last = +new Date();
-    var tick = function() {
-      el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
-      last = +new Date();  
-      if (+el.style.opacity < 1) {
-        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
-      } 
-    };  
-    tick();
-}
 
 var menuRetratilPosicaoAberta = false;
 function alternarMenuRetratil(){
-    var menuLateral = document.getElementById('menu-lateral');
-    var tituloMenuRetratilA = document.getElementById('titulo-menu-retratil').children[0];
     if (menuRetratilPosicaoAberta){
-        menuLateral.style.display = 'none';
-        tituloMenuRetratilA.style.backgroundPositionY = 'initial';
+        document.body.classList.remove('menu-lateral-aberto');
     } else {
-        fadeIn(menuLateral);
-        tituloMenuRetratilA.style.backgroundPositionY = '-73px';
+        document.body.classList.add('menu-lateral-aberto');
     }    
     menuRetratilPosicaoAberta = !menuRetratilPosicaoAberta;
 }
